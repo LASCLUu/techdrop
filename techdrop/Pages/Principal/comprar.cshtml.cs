f513913df71e2fbd71574b5369b8ce680f029e65
Lucas Augustho <augusthocandido3@outlook.com>
2024-04-17 21:16:39 -0300

    Adicionar arquivos de projeto.

---
+++ techdrop/Pages/Principal/comprar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace techdrop.Pages.Principal
+{
+    public class comprarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ techdrop/Pages/Principal/produtos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace techdrop.Pages.Principal
+{
+    public class produtosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
